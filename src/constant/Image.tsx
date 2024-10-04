fb5b94bbe5a8eddff5703352e1e77844b7f2eed8
plaze <banikakash131@gmail.com>
2024-10-04 11:48:59 +0530

    feat: add team details

--- src/constant/Image.tsx
+++ src/constant/Image.tsx
@@ -1,4 +1,7 @@
 import krishna from "./../assets/krishna.jpg";
 import akash from "./../assets/akash.png";
+import gyandeep from "./../assets/gyandeep.jpeg";
+import abhishek from "./../assets/Abhishek.jpeg";
+import subrajit from "./../assets/Subhrajit.jpg";
 
-export { krishna, akash };
+export { krishna, akash, gyandeep, abhishek, subrajit };
